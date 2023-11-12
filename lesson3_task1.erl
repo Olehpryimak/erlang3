d4cfd014b6faa9b83014230f4d0c4708becb9a61
olehpryimak <o380979848493o@gmail.com>
2023-11-12 17:58:24 +0000

    first word utf8 fix

--- lesson3_task1.erl
+++ lesson3_task1.erl
@@ -5,7 +5,7 @@ first_word(S) -> first_word(trim(S), <<>>).
 
 first_word(<<>>, Res) -> Res;
 first_word(<<" ", _/binary >>, Res) -> Res;
-first_word(<<Char/utf8, Rest/binary>>, Res) -> first_word(Rest, <<Res/binary, Char>>).
+first_word(<<Char/utf8, Rest/binary>>, Res) -> first_word(Rest, <<Res/binary, Char/utf8>>).
 
 trim(<<" ", Rest/binary>>) -> trim(Rest);
 trim(A) -> A.
